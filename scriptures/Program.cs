771568b7dd83dde278797b2291f8ba18fce63770
gloria <gloria27ekpo@gmail.com>
2023-05-20 22:44:31 -0600

    scritures

---
+++ scriptures/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Word
+{
+    public string Text { get; }
+    public bool Hidden { get; private set; }
+
+    public Word(string text)
+    {
+        Text = text;
+        Hidden = false;
+    }
+
+    public void Hide()
+    {
+        Hidden = true;
+    }
+
+    public override string ToString()
+    {
+        return Hidden ? "______" : Text;
+    }
+}
+
